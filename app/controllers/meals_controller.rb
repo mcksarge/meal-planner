00409d565724177b72c91e0bcb14152b5984f7a3
mcksarge <higgins.fred.s@gmail.com>
2022-07-17 15:54:32 -0700

    Add POST functionality for Meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,4 +6,19 @@ class MealsController < ApplicationController
         render json: meals
     end
 
+    def create
+        meal = Meal.create(meal_params)
+        if meal.valid?
+            render json: meal, status: :created
+        else
+            render json: {errors: ["Invalid Data"]}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def meal_params
+        params.permit(:name, :recipe, :cooking_time, :image)
+    end
+
 end
--- client/src/components/MealList.js
+++ client/src/components/MealList.js
@@ -4,12 +4,19 @@ import {useState, useEffect} from "react";
 
 function Meals(){
     const [allMeals, setAllMeals] = useState([])
+    const [refreshMeals, setRefreshMeals] = useState(true)
+    const [showAddMeal, setShowAddMeal] = useState(false)
+    const [name, setName] = useState("")
+    const [recipe, setRecipe] = useState("")
+    const [image, setImage] = useState("")
+    const [cooking_time, setCooking_time] = useState("")
 
     useEffect(() => {
+        setRefreshMeals(false)
         fetch("http://localhost:3000/meals")
             .then((res) => res.json())
             .then((meals) => setAllMeals(meals))
-    }, [])
+    }, [refreshMeals])
 
     let meals = allMeals.map((meal, i) => {
         return (
@@ -21,14 +28,60 @@ function Meals(){
             />
         )
     })
+
+    function handleNewMeal() {
+        setShowAddMeal(true)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        fetch('/meals', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                cooking_time,
+                recipe,
+                image
+            })
+        })
+        .then((res) => res.json())
+        .then((newMeal) => setRefreshMeals(true))
+    }
     
 
-
-    return (
-        <div id="meal-list">
-            {meals}
-        </div>
-    )
+    if(!showAddMeal){
+        return (
+            <>
+                <button id="new-recipe-btn" onClick={handleNewMeal}>Add Recipe</button>
+                <div id="meal-list">
+                    {meals}
+                </div>
+            </>
+        )
+    }else{
+        return (
+            <>
+                <button id="new-recipe-btn" onClick={handleNewMeal}>Add Recipe</button>
+                <div id="new-recipe-div">
+                    <form onSubmit={handleSubmit}>
+                        <input placeholder="Meal name" onChange={(e) => setName(e.target.value)}></input>
+                        <input placeholder="Cooking Time" onChange={(e) => setCooking_time(e.target.value)}></input>
+                        <input placeholder="Recipe" onChange={(e) => setRecipe(e.target.value)}></input>
+                        <input placeholder="Image" onChange={(e) => setImage(e.target.value)}></input>
+                        <button id="add-recipe-btn">Submit</button>
+                    </form>
+                </div>
+                <div id="meal-list">
+                    {meals}
+                </div>
+            </>
+        )
+    }
+    
 }
 
 export default Meals
