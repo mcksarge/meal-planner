1775322df4ff9c3353c3abff1ed8d4746db19a96
mcksarge <higgins.fred.s@gmail.com>
2022-07-19 16:51:24 -0700

    Add review section for meal recipes

---
+++ client/src/components/MealReview.js
@@ -0,0 +1,14 @@
+
+
+function MealReview({key, rating, review, user}) {
+    console.log(user)
+
+    return (
+        <div className="review-div">
+            <h5>{review}</h5>
+            <p></p>
+        </div>
+    )
+}
+
+export default MealReview
--- client/src/components/RecipePage.js
+++ client/src/components/RecipePage.js
@@ -1,7 +1,10 @@
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
+import {useState} from 'react';
+import MealReview from './MealReview';
 
 function RecipePage({meals}) {
     let params = useParams()
+    
 
     let currentId = params.id
     
@@ -9,14 +12,31 @@ function RecipePage({meals}) {
         return meal.id == currentId
     })
 
+    let review = currentMeal.reviews
+    console.log(review)
+
+    let mealReviews = review.map((review, i) => {
+        return (
+            <MealReview key={i} rating={review.rating} review={review.review} user={review.user} />
+        )
+    })
+
+
 
     return (
         <div>
             <p>Hello Recipes!  Get your recipes here!</p>
             <h1>{currentMeal.name}</h1>
+            <h3>Cooking Time: {currentMeal.cooking_time} minutes</h3>
             <img src={currentMeal.image} className="meal-image"></img>
             <h2>Recipe:</h2>
             <p>{currentMeal.recipe}</p>
+            <button>Add a review</button>
+            <div className="meal-review">
+                <div>
+                    {mealReviews}
+                </div>
+            </div>
         </div>
     )
 }
